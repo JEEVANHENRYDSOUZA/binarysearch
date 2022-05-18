c47b521688d7b636978a11d6f27b71f47030abf6
Jeevan Henry Dsouza <77383748+JEEVANHENRYDSOUZA@users.noreply.github.com>
2022-05-18 16:10:45 -0400

    Create binarysearch2darray.py

---
+++ 2-d array binary search/binarysearch2darray.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        start=0
+        end=len(matrix[0])-1
+        while(end>=0 and start <=len(matrix)-1):
+            mid=start+(end-start)/2
+            if(matrix[start][end]==target):
+                return True
+            elif(matrix[start][end]<target):
+                start+=1
+                
+                
+            else:
+                end-=1
+        return False
